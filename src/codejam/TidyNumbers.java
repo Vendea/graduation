c2f7ca478e4c9e7553ee100370ccd74b84dacbea
Vendea <kbeine@live.com>
2017-04-07 23:42:39 -0500

    new repo, start google codejam qual round

---
+++ src/codejam/TidyNumbers.java
@@ -0,0 +1,14 @@
+package codejam;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TidyNumbers {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
